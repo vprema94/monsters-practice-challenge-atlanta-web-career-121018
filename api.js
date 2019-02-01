6dc8ab36b1c55debaaa9745a6c8486e2d264c3a2
Vanessa Prema <vanessa.prema@flatironschool.com>
2019-02-01 16:36:15 -0500

    DONE

--- api.js
+++ api.js
@@ -1,10 +1,10 @@
-function getMonster() {
-    let url = 'http://localhost:3000/monsters';
+function getMonster() { 
+    let url = `http://localhost:3000/monsters?_limit=50&_page=${page}`;
     return fetch(url).then(res => res.json())
   }
   
   function createMonster(name, age, description) {
-    return fetch('http://localhost:3000/pokemon', {
+    return fetch(`http://localhost:3000/monsters`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", setupPage)
+let page = 1;
+let container = document.querySelector("#monster-container")
 
 function setupPage() {
-//   addFormHandler()
+  addFormHandler()
+  addButtonHander()
   renderAllMonsters()
 }
 
@@ -15,17 +18,34 @@ function addMonster (event) {
 
 function addFormHandler() {
   let form = document.querySelector("form");
-  form.addEventListener('submit', addMonster)
-}
+  form.addEventListener('submit', addMonster) 
+} 
 
-function renderAllMonsters() {
-  getMonster().then(function (data) {
-    data.forEach(renderMonster)
-  })
+function renderAllMonsters() { 
+    container.innerHTML = "" 
+    getMonster().then(function (data) {
+        data.forEach(renderMonster)
+    })
 }
 
 function renderMonster(monster) {
-  let container = document.querySelector("#monster-container")
   let element = monsterView(monster)
   container.appendChild(element)
+}  
+
+function addButtonHander() {
+    let bkd = document.querySelector("#back")
+    bkd.addEventListener('click', goBack)
+    let fwd = document.querySelector("#forward")
+    fwd.addEventListener('click', goFwd)
+}
+
+function goFwd() {
+    page++ 
+    renderAllMonsters()
+} 
+
+function goBack() {
+    page--
+    renderAllMonsters()
 }
