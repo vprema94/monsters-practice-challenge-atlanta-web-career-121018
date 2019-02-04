19923cfd0779b0e5a1f4a2d21c9c22688cce0300
Vanessa Prema <vanessa.prema@flatironschool.com>
2019-02-03 19:24:55 -0500

    reorganized code

--- index.js
+++ index.js
@@ -6,31 +6,35 @@ function setupPage() {
   addFormHandler()
   addButtonHander()
   renderAllMonsters()
-}
+} 
+ 
+function addFormHandler() {
+    let form = document.querySelector("form");
+    form.addEventListener('submit', addMonster) 
+}  
 
 function addMonster (event) {
-  event.preventDefault();
-  let name = event.target.name.value
-  let age = event.target.age.value
-  let description = event.target.description.value
-  createMonster(name, age, description).then(renderMonster)
-}
+    event.preventDefault();
+    let name = event.target.name.value
+    let age = event.target.age.value
+    let description = event.target.description.value
+    createMonster(name, age, description).then(renderMonster)
+}  
 
-function addFormHandler() {
-  let form = document.querySelector("form");
-  form.addEventListener('submit', addMonster) 
-} 
-
-function renderAllMonsters() { 
-    container.innerHTML = "" 
-    getMonster().then(function (data) {
-        data.forEach(renderMonster)
+function createMonster(name, age, description) {
+    return fetch(`http://localhost:3000/monsters`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            age: age,
+            description: description
+        })
+    }).then(function (response) {
+        return response.json()
     })
-}
-
-function renderMonster(monster) {
-  let element = monsterView(monster)
-  container.appendChild(element)
 }  
 
 function addButtonHander() {
@@ -38,8 +42,8 @@ function addButtonHander() {
     bkd.addEventListener('click', goBack)
     let fwd = document.querySelector("#forward")
     fwd.addEventListener('click', goFwd)
-}
-
+} 
+ 
 function goFwd() {
     page++ 
     renderAllMonsters()
@@ -48,4 +52,40 @@ function goFwd() {
 function goBack() {
     page--
     renderAllMonsters()
+}
+
+function renderAllMonsters() { 
+    container.innerHTML = "" 
+    getMonster().then(function (data) {
+        data.forEach(renderMonster)
+    })
+} 
+
+function getMonster() { 
+    let url = `http://localhost:3000/monsters?_limit=50&_page=${page}`;
+    return fetch(url).then(res => res.json())
+}  
+
+function renderMonster(monster) {
+    let element = monsterView(monster)
+    container.appendChild(element)
+} 
+
+function monsterView(monster) {
+    let element = document.createElement('div')
+    element.className = 'card' // element.classList.push('card')
+    element.dataset.id = monster.id
+        let monName = document.createElement('h2')
+        monName.textContent = monster.name
+        monName.className = 'center-text'
+        element.appendChild(monName) 
+
+        let monAge = document.createElement('h4')
+        monAge.textContent = "Age: " + monster.age
+        element.appendChild(monAge)
+
+        let monDes = document.createElement('p')
+        monDes.textContent = "Bio: " + monster.description
+        element.appendChild(monDes)
+    return element
 }
